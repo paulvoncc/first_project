d18acb2c301d13cb590a5693e9806181e02edd09
paulvoncc <paulvon@qq.com>
2016-03-27 08:41:25 +0800

    This is my first project github

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # first_project
 Some test files
+
+This is my first project in the github...
---
+++ test_find.lua
@@ -0,0 +1,66 @@
+
+
+req_uri1 = "http://hahah/crossdomain.xml"
+req_uri2 = "/crossdomain.xml"
+req_uri3 = "/hahaha/crossdomain.xml"
+req_uri4 = "/hahaha/hehe/crossdomain.xml"
+req_uri5 = "/hahaha/hehe/crossdomain.mp3"
+
+print("1")
+	if string.find(req_uri1, "^/crossdomain%.xml") ~= nil then
+		print(req_uri1.." match")
+	else
+		print(req_uri1.." not match")
+	end
+print("2")
+	if string.find(req_uri2, "^/crossdomain%.xml") ~= nil then
+		print(req_uri2.." match")
+	else
+		print(req_uri2.." not match")
+	end
+print("3")
+	if string.find(req_uri3, "^%/crossdomain%.xml") ~= nil then
+		print(req_uri3.." match")
+	else
+		print(req_uri3.." not match")
+	end
+print("4")
+	if string.find(req_uri4, "^%/crossdomain%.xml") ~= nil then
+		print(req_uri4.." match")
+	else
+		print(req_uri4.." not match")
+	end
+print("5")
+	if string.find(req_uri5, "^%/crossdomain%.xml") ~= nil then
+		print(req_uri5.." match")
+	else
+		print(req_uri5.." not match")
+	end
+print("6")
+	if string.find("/crossdomain.xml", "^%/crossdomain%.xml") ~= nil then
+		print("/crossdomain.xml".." match")
+	else
+		print("/crossdomain.xml".." not match")
+	end
+print("7")
+	if string.find("/crossdomain.xml", "^/crossdomain.xml") ~= nil then
+		print("/crossdomain.xml".." match")
+	else
+		print("/crossdomain.xml".." not match")
+	end
+print("8")
+	if string.find("/crossdomain.xml", "^/crossdomain%.xml") ~= nil then
+		print("/crossdomain.xml".." match")
+	else
+		print("/crossdomain.xml".." not match")
+	end
+
+
+local cur_time = os.date("%Y-%m-%dT%H:%M:%SZ")
+print(cur_time)
+
+cur_time = os.date("!%Y-%m-%dT%H:%M:%SZ")
+print(cur_time)
+
+
+
